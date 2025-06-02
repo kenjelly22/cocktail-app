eea0b883228a318cf1aaeb7403607634aa7a8d6c
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-02 14:59:18 -0400

    Test API to confirm it will return requested data

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const searchBtn = document.querySelector("#search-button")
 const userInput = document.querySelector("#database-search").value
 
 const searchCocktails = () => {
-  userInput
-  searchBtn.addEventListener("click", () => console.log(userInput))
+  // userInput
+  searchBtn.addEventListener("click", () => {
+    fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=moscow_mule"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const drinkName = res.drinks[0].strDrink
+        console.log(drinkName)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error)
+      })
+  })
 }
 
 searchCocktails()
